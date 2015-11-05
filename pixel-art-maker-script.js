d11c527ad3f61660e26d1690e228f61181b6e501
AriFlo <ariflo@arianflores.com>
2015-11-04 20:42:37 -0800

    added color palette to pixel art maker

--- pixel-art-maker-script.js
+++ pixel-art-maker-script.js
@@ -1,9 +1,14 @@
 	var body = document.getElementsByTagName("body") [0];
-	var abcArray = ["abcdefghijklmnopqrstuvwxyz"].toString();
-	var numArray = ["123456789"].toString();
+	var uptick = 0;
+	var uptick2 = 144;
+	var uptick3 = 10;
+	randomNum1 = Math.floor(Math.random()*120);
+	randomNum2 = Math.floor(Math.random()*110);
+	randomNum3 = Math.floor(Math.random()*200);
+	randomNum4 = Math.floor(Math.random()*115);
 
 
-	for(var i = 0; i<=6496; i++){
+	for(var i = 0; i<=6497; i++){
 
 		var square = document.createElement("div");
 		body.appendChild(square);
@@ -17,11 +22,10 @@
 		square.style.boxShadow = ' 0px 0px 25px #9E8F8F';
 	};
 
-	for(var i = 0; i <=72; i++){
-		var uptick = 0;
-		uptick.toString(); 
-
-
+	for(var i = 0; i <=10; i++){
+		
+		square.style.background = "rgb( " + uptick + ", " + uptick + ", " + uptick + ")"; 
+		uptick += 8; 
 		var square = document.createElement("div");
 		body.appendChild(square);
 		square.style.border= '2px solid  #000000';
@@ -29,12 +33,34 @@
 		square.style.float = 'left';
 		square.style.paddingBottom = '1%';
 		square.style.margin = '.10%';
-		square.style.boxShadow = ' 0px 0px 25px #9E8F8F';
+		square.style.boxShadow = ' 0px 0px 25px #9E8F8F';	
+	};
 
-		square.style.background = "rgb(0, 0, 0)";
-		uptick += 8; 
+	for(var i = 0; i <=25; i++){
 		
+		square.style.background = "rgb( " + uptick2 + ", " + randomNum1 + ", " + randomNum2 + ")"; 
+		uptick2 += 8; 
+		var square = document.createElement("div");
+		body.appendChild(square);
+		square.style.border= '2px solid  #000000';
+		square.style.width = '1%';
+		square.style.float = 'left';
+		square.style.paddingBottom = '1%';
+		square.style.margin = '.10%';
+		square.style.boxShadow = ' 0px 0px 25px #9E8F8F';	
+	};
 
+	for(var i = 0; i <=34; i++){
 		
+		square.style.background = "rgb( " + uptick3 + ", " + randomNum3 + ", " + randomNum4 + ")"; 
+		uptick3 += 10; 
+		var square = document.createElement("div");
+		body.appendChild(square);
+		square.style.border= '2px solid  #000000';
+		square.style.width = '1%';
+		square.style.float = 'left';
+		square.style.paddingBottom = '1%';
+		square.style.margin = '.10%';
+		square.style.boxShadow = ' 0px 0px 25px #9E8F8F';	
 	};
 
